26c3eb6021a7b91a92d27e096c49a84086ca7b1a
Mark Ratjens <mark@ratjens.com>
2019-03-20 11:49:26 +1100

    Initial Commit

---
+++ Yarngun/Yarngun.h
@@ -0,0 +1,19 @@
+//
+//  Yarngun.h
+//  Yarngun
+//
+//  Created by Mark Ratjens on 20/3/19.
+//  Copyright © 2019 Nyasa Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Yarngun.
+FOUNDATION_EXPORT double YarngunVersionNumber;
+
+//! Project version string for Yarngun.
+FOUNDATION_EXPORT const unsigned char YarngunVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Yarngun/PublicHeader.h>
+
+
